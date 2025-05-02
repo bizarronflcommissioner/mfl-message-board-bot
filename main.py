93762f913736ca8bf7b9cfe7fcb13e8e1cf345a5
bizarronflcommissioner <bizarronflcommissioner@gmail.com>
2025-05-01 21:57:14 -0700

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import discord
 from discord.ext import commands
 import aiohttp
 import os
-from dotenv import load_dotenv
+from dotenv import load_dotenV
 
 load_dotenv()
 DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
